91282882991792274dd2b37716fc428645841f94
Your Name <you@example.com>
2023-11-16 21:23:05 +0530

    1st

---
+++ index.js
@@ -0,0 +1,16 @@
+const connectTOMongo = require("./db");
+const { body, validationResult } = require("express-validator");
+const express = require("express");
+const cors = require("cors");
+
+connectTOMongo();
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.listen(port, () => {
+  console.log(`iNoteBook backend listening on port ${port}`);
+});
